b924e86e24c20a935c053b1be46059b8d64d2532
Truc Chau <ititiu20327@student.hcmiu.edu.vn>
2021-11-17 23:22:13 +0700

    modify MasterEntry

--- MasterEntry.h
+++ MasterEntry.h
@@ -7,11 +7,11 @@
 class MasterEntry
 {
 public:
-	virtual void AddNewMember();
-	virtual void ShowMember();
-	virtual void UpdateRecord();
-	virtual void DeleteRecord();
-	virtual void SearchRecord();
-	virtual void Menu();
+	virtual void AddNewMember() = 0;
+	virtual void ShowMember() = 0;
+	virtual void UpdateRecord() = 0;
+	virtual void DeleteRecord() = 0;
+	virtual void SearchRecord() = 0;
+	virtual void Menu() = 0;
 };
 #endif // !MASTER_ENTRY_H_
